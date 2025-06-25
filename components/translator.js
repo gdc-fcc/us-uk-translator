aafc8974b277d84cb7c7fa0a49e714a80cab5cc9
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-25 22:44:59 +0200

    pass functional tests for US -> UK

--- components/translator.js
+++ components/translator.js
@@ -3,8 +3,31 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
-class Translator {
+const pc2 = (str) => {
+    return [...str].map((x,i) => i == 0 ? x.toUpperCase(): x).join('')
+}
+const hl = str => '<span class="highlight">' + str + '</span>'
 
+class Translator {
+  toUK(text) {
+    for (const [pattern, replacement] of Object.entries(americanOnly)) {
+        const regex = new RegExp('\\b' + pattern + '\\b', 'i')
+        text = text.replace(regex, hl(replacement))
+    }
+    for (const [pattern, replacement] of Object.entries(americanToBritishSpelling)) {
+        const regex = new RegExp('\\b' + pattern + '\\b', 'i')
+        text = text.replace(regex, hl(replacement))
+    }
+    for (const [pattern, replacement] of Object.entries(americanToBritishTitles)) {
+        const regex = new RegExp(pattern.replace('.', '\\.'))
+        text = text.replace(regex, hl(replacement))
+        const regex2 = new RegExp(pc2(pattern).replace('.', '\\.'))
+        text = text.replace(regex2, hl(pc2(replacement)))
+    }
+    const re = /\d+:\d+/
+    text = text.replace(re, match => hl(match.replace(':', '.')))
+     return text;
+  }
 }
 
 module.exports = Translator;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,7 +2,69 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Translator = require('../components/translator.js');
+const translator = new Translator()
 
 suite('Unit Tests', () => {
-
+    suite('To British', () => {
+        test('Mangoes are my favorite fruit.', () => {
+            assert.equal(
+                translator.toUK('Mangoes are my favorite fruit.'),
+                'Mangoes are my <span class="highlight">favourite</span> fruit.'
+            )
+        })
+        test('I ate yogurt for breakfast.', () => {
+            assert.equal(
+                translator.toUK('I ate yogurt for breakfast.'),
+                'I ate <span class="highlight">yoghurt</span> for breakfast.'
+            )
+        })
+        test('We had a party at my friend\'s condo.', () => {
+            assert.equal(
+                translator.toUK('We had a party at my friend\'s condo.'),
+                'We had a party at my friend\'s <span class="highlight">flat</span>.'
+            )
+        })
+        test('Can you toss this in the trashcan for me?', () => {
+            assert.equal(
+                translator.toUK('Can you toss this in the trashcan for me?'),
+                'Can you toss this in the <span class="highlight">bin</span> for me?'
+            )
+        })
+        test('The parking lot was full.', () => {
+            assert.equal(
+                translator.toUK('The parking lot was full.'),
+                'The <span class="highlight">car park</span> was full.'
+            )
+        })
+        test('Like a high tech Rube Goldberg machine.', () => {
+            assert.equal(
+                translator.toUK('Like a high tech Rube Goldberg machine.'),
+                'Like a high tech <span class="highlight">Heath Robinson device</span>.'
+            )
+        })
+        test('To play hooky means to skip class or work.', () => {
+            assert.equal(
+                translator.toUK('To play hooky means to skip class or work.'),
+                'To <span class="highlight">bunk off</span> means to skip class or work.'
+            )
+        })
+        test('No Mr. Bond, I expect you to die.', () => {
+            assert.equal(
+                translator.toUK('No Mr. Bond, I expect you to die.'),
+                'No <span class="highlight">Mr</span> Bond, I expect you to die.'
+            )
+        })
+        test('Dr. Grosh will see you now.', () => {
+            assert.equal(
+                translator.toUK('Dr. Grosh will see you now.'),
+                '<span class="highlight">Dr</span> Grosh will see you now.'
+            )
+        })
+        test('Lunch is at 12:15 today.', () => {
+            assert.equal(
+                translator.toUK('Lunch is at 12:15 today.'),
+                'Lunch is at <span class="highlight">12.15</span> today.'
+            )
+        })
+    })
 });
