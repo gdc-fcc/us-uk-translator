7b07dfd678308c5f1b9a15c56e5720103d849e72
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-26 15:26:23 +0200

    impleent functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,8 +5,95 @@ const server = require('../server.js');
 
 chai.use(chaiHttp);
 
-let Translator = require('../components/translator.js');
+//let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
-
+    suite('Translation with...', () => {
+        test('text and locale fields', (done) => {
+            chai
+                .request(server)
+                .keepOpen()
+                .post('/api/translate')
+                .send({ locale: 'american-to-british', text: 'Dr. Grosh will see you now.' })
+                .end((_err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.equal(
+                        res.body.translation,
+                        '<span class="highlight">Dr</span> Grosh will see you now.'
+                    )
+                    done()
+                })
+        })
+        test('text and invalid locale field', (done) => {
+            chai
+                .request(server)
+                .keepOpen()
+                .post('/api/translate')
+                .send({ locale: 'american-to-german', text: 'Dr. Grosh will see you now.' })
+                .end((_err, res) => {
+                    assert.equal(
+                        res.body.error,
+                        'Invalid value for locale field'
+                    )
+                    done()
+                })
+        })
+        test('missing text field', done => {
+            chai
+                .request(server)
+                .keepOpen()
+                .post('/api/translate')
+                .send({ locale: 'american-to-british' })
+                .end((_err, res) => {
+                    assert.equal(
+                        res.body.error,
+                        'Required field(s) missing'
+                    )
+                    done()
+                })
+        })
+        test('missing locale field', (done) => {
+            chai
+                .request(server)
+                .keepOpen()
+                .post('/api/translate')
+                .send({ text: 'Dr. Grosh will see you now.' })
+                .end((_err, res) => {
+                    assert.equal(
+                        res.body.error,
+                        'Required field(s) missing'
+                    )
+                    done()
+                })
+        })
+        test('empty text', (done) => {
+            chai
+                .request(server)
+                .keepOpen()
+                .post('/api/translate')
+                .send({ locale: 'american-to-british', text: '' })
+                .end((_err, res) => {
+                    assert.equal(
+                        res.body.error,
+                        'No text to translate'
+                    )
+                    done()
+                })
+        })
+        test('text that needs no translation', (done) => {
+            chai
+                .request(server)
+                .keepOpen()
+                .post('/api/translate')
+                .send({ locale: 'american-to-british', text: 'Does not need tranlation' })
+                .end((_err, res) => {
+                    assert.equal(res.status, 200)
+                    assert.equal(
+                        res.body.translation,
+                        'Everything looks good to me!'
+                    )
+                    done()
+                })
+        })
+    })
 });
