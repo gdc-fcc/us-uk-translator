6cb0c6e8964082532fc44a5986c25a0c713fd544
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-26 14:50:12 +0200

    finalize API
    only functinal tests ( and last unit test) missing

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Translator = require('../components/translator.js');
+const locales = ["american-to-british", "british-to-american"]
 
 module.exports = function (app) {
   
@@ -8,6 +9,21 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const text = req.body.text;
+      const locale = req.body.locale;
+      if (text===undefined || locale === undefined) {
+        res.json({ error: 'Required field(s) missing' })
+      } else if (typeof text !== "string" || text.length === 0) {
+        res.json({ error: 'No text to translate' })
+      } else if (typeof locale !== "string" || !locales.includes(locale)) {
+        res.json({ error: 'Invalid value for locale field' })
+      } else {
+        const translated = locale === locales[0] ? translator.toUK(text) : translator.toUS(text)
+        if (translated === text) {
+          res.json({text, translation: "Everything looks good to me!"})
+        } else {
+          res.json({text, translation: translated})
+        }
+      }
     });
 };
